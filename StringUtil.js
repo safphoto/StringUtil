ab1136f6e9d93ae0451d1cbd97dfbaad261c4ac8
Scott Flaherty <Scott.Flaherty@mckesson.com>
2015-01-02 15:52:14 -0500

    documenting return types

--- StringUtil.js
+++ StringUtil.js
@@ -1,6 +1,9 @@
 var stringUtil = {};
 
 (function(util) {
+    /**
+     * @return {string}
+     */
     util.Format = function() {
         var s = arguments[0];
         
@@ -10,30 +13,48 @@ var stringUtil = {};
         
         return s;
     };
-    
+
+    /**
+     * @return {string}
+     */
     util.ReplaceAll = function (source, target, replaceWith) {
         return source.split(target).join(replaceWith);
     };
-    
+
+    /**
+     * @return {string}
+     */
     util.IsNullOrEmpty = function(value) {
         return !value;
     };
-    
+
+    /**
+     * @return {string}
+     */
     util.Capitalize = function(source) {
         var lc = source.toLowerCase();
         return lc.replace( /(^|\s)([a-z])/g , function(m,p1,p2){return p1+p2.toUpperCase();});
     };
-    
+
+    /**
+     * @return {string}
+     */
     util.ConvertJsonToDateTime = function(jsonDateTime) {
         var milli = jsonDateTime.replace(/\/Date\((-?\d+)\)\//, '$1');
         var date = new Date(parseInt(milli));
         return date;
     };
-    
+
+    /**
+     * @return {string}
+     */
     util.Contains = function(source, target, isCaseSensitive) {
         return !!new RegExp(target, !isCaseSensitive ? "i" : "" + "g").test(source);
     };
-    
+
+    /**
+     * @return {string}
+     */
     util.Remove = function(source, start, length) {
         var s = '';
         
@@ -45,35 +66,59 @@ var stringUtil = {};
           
         return s;
     };
-    
+
+    /**
+     * @return {string}
+     */
     util.Repeat = function (source, count) {
         return new Array( count + 1 ).join( source );
     };
-    
+
+    /**
+     * @return {string}
+     */
     util.ReplaceAll = function(source, find, replace) {
         return source.split(find).join(replace);
     };
-    
+
+    /**
+     * @return {Array.<string>}
+     */
     util.ToCharArray = function(source) {
         return source.split('');
     };
-    
+
+    /**
+     * @return {Array.<string>}
+     */
     util.ToWordArray = function(source) {
         return source.split(' ');
     };
-    
+
+    /**
+     * @return {string}
+     */
     util.Trim = function (source) {
         return source.replace(/^\s+|\s+$/g, "");
     };
-    
+
+    /**
+     * @return {string}
+     */
     util.TrimLeft = function(source) {
         return source.replace(/^\s+/, "");
     };
-    
+
+    /**
+     * @return {string}
+     */
     util.TrimRight = function(source) {
         return source.replace(/\s+$/, '');
     };
-    
+
+    /**
+     * @return {string}
+     */
     util.Highlight = function(text, words, tag) {
       // Default tag if no tag is provided
       tag = tag || 'span';
@@ -91,7 +136,10 @@ var stringUtil = {};
      
       return text;
     };
-    
+
+    /**
+     * @return {string}
+     */
     util.Unhighlight = function(text, tag) {
       // Default tag if no tag is provided
       tag = tag || 'span';
